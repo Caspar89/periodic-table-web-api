4018c2a0715b258993b8cee7cece5c435c82a89f
vandeursenc <c.vandeursen@elsevier.com>
2020-01-03 16:28:37 +0000

    Changed the string text for the ping

--- src/main/java/com/element/app/controller/PeriodicTableController.java
+++ src/main/java/com/element/app/controller/PeriodicTableController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class PeriodicTableController {
     @RequestMapping("/ping")
     public String ping() {
-        return "Periodic table controller is alive and kicking!";
+        return "Periodic table web api is alive and kicking!";
     }
 }
